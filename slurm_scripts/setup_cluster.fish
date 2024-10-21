1f674b398b58fd202e67e492fe91aa5692607525
konakarthik12 <koankarthik12@gmail.com>
2024-10-21 10:09:10 -0400

    changes

---
+++ slurm_scripts/setup_cluster.fish
@@ -0,0 +1,16 @@
+#!/usr/bin/env fish
+set setup_dir $SCRATCH_DIR
+if test (count $argv) -ge 1
+    set setup_dir $argv[1]
+end
+echo "Setting up cluster in $(realpath $setup_dir)"
+. /fs/nexus-projects/KGB-MBRL/scratch_hold/setup_all.fish $setup_dir
+
+pushd (dirname (status -f))
+cd ..
+
+echo "Generating tdmpc2 conda environment"
+conda env remove -n isaaclab
+
+popd
+echo "Cluster is ready to run tdmpc2"
