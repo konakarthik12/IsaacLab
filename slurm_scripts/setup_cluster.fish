545ada140fc6720a6b598094b858775a5383ed6b
konakarthik12 <koankarthik12@gmail.com>
2024-11-01 13:18:16 -0400

    avoid pip warnings
    (cherry picked from commit e9f48558d65654c57238b47f4179d22715ee8fae)

--- slurm_scripts/setup_cluster.fish
+++ slurm_scripts/setup_cluster.fish
@@ -10,6 +10,9 @@ set isaac_env "isaac-sim-$SLURM_JOB_ID"
 echo "Generating isaac lab conda environment: $isaac_env"
 ./isaaclab.sh --conda $isaac_env
 conda activate $isaac_env
-python -m pip install -U pip
+
+python -m pip install -U pip # pip outdated warnings
+python -m pip install "usd-core<24.00" "lxml<5.0.0" "tqdm<5.0.0" # pip resolver warnings
+
 ./isaaclab.sh --install rsl-rl
 popd
