b54f2fee6a4aa72fb08f672ff19b2747039cee1d
Karthikeya Kona <konakarthik12@gmail.com>
2025-03-13 11:09:58 -0400

    alien crab

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/crab/crab_env.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/crab/crab_env.py
@@ -23,7 +23,7 @@ class CrabEnvCfg(DirectRLEnvCfg):
     # env
     episode_length_s = 15.0
     decimation = 4
-    action_scale = 0.5 * 1e-4 * 1e-3
+    action_scale = 0.5 * 1e-5
     action_space = 18
     observation_space = 66
     state_space = 0
